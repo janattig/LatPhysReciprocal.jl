0338db472316ab98f14d18ea20d866eb102526e7
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-19 11:25:57 +0100

    inserted module scope into the main src file

--- src/LatPhysReciprocal.jl
+++ src/LatPhysReciprocal.jl
@@ -1,5 +1,35 @@
+################################################################################
+#
+#   module LatPhysReciprocal
+#   -> LatticePhysics_Base
+#   -> LinearAlgebra
+#
+#   --> TYPE DEFINITIONS
+#           - Path
+#           - BrillouinZone
+#
+#   --> PATHS IN MOMENTUM SPACE
+#           - modification of paths
+#           - pre-implemented paths
+#           - paths based on Unitcells
+#
+#   --> BRILLOUIN ZONES
+#           - type definition
+#           - pre-implemented BZs
+#           - construction of default BZs (2D)
+#           - construction of default BZs (3D)
+#
+################################################################################
+
+
+
+
+# module start
 module LatPhysReciprocal
 
-greet() = print("Hello World!")
 
-end # module
+
+
+
+# module end
+end
