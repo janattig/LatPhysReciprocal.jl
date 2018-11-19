2e5bac3856a8e21ce1e93061baa9c30a612c60f6
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-19 11:19:11 +0100

    Files generated by PkgTemplates

---
+++ src/LatPhysReciprocal.jl
@@ -0,0 +1,5 @@
+module LatPhysReciprocal
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LatPhysReciprocal
+using Test
+
+@testset "LatPhysReciprocal.jl" begin
+    # Write your own tests here.
+end
