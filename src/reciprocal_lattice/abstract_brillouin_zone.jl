ef8310ef97c76061d48a8df4dae2378d4a6c72ac
Jan <jattig@thp.uni-koeln.de>
2018-11-23 17:22:21 +0100

    added wishlist for functions to be implemented

---
+++ src/reciprocal_lattice/abstract_brillouin_zone.jl
@@ -0,0 +1,8 @@
+
+# FUNCTIONS ON THE WISHLIST
+#
+# - corners(BZ), faces(BZ), edges(BZ)
+# - rec_unitcell(BZ)
+#
+# - getBrillouinZone(rec_unitcell)
+# - getBrillouinZoneCorners(rec_unitcell)   (... edges, faces...)
