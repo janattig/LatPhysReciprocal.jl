745eb9bc8b676e2047fecd4ddb37ba72a4b2442a
Jan <jattig@thp.uni-koeln.de>
2018-11-24 09:01:54 +0100

    more interface functions for reciprocal unitcells

--- src/reciprocal_lattice/abstract_reciprocal_unitcell.jl
+++ src/reciprocal_lattice/abstract_reciprocal_unitcell.jl
@@ -2,7 +2,7 @@
 #
 #	ABSTRACT TYPE
 #
-#   ReciprocalUnitcell{S,B} <: Unitcell{P,B}
+#   ReciprocalUnitcell{P,B} <: Unitcell{P,B}
 #   --> P is the reciprocal site type (<: AbstractReciprocalPoint{D})
 #       --> D is the dimension of embedding space
 #   --> B is the bond type (<: AbstractBond{L,N})
@@ -53,10 +53,37 @@ function newUnitcell(
 
     # check if only a single site is given
     @assert length(sites) == 1
+    # check if the site is located at 0
+    @assert sum(abs.(point(sites[1]))) < 1e-8
 
     # pass to the reciprocal unitcell constructor interface
-
+    return newReciprocalUnitcell(U, lattice_vectors, bonds)
 end
 
 # export the newUnitcell interface
 export newUnitcell
+
+
+
+
+
+
+
+################################################################################
+#
+#	INTERFACING / ACCESSING RECIPROCAL UNITCELLS
+#	(functions that have to be overwritten by concrete types)
+#
+################################################################################
+
+# constructor
+function newReciprocalUnitcell(
+            ::Type{R},
+            lattice_vectors :: Vector{<:Vector{<:Real}},
+            bonds           :: Vector{B}
+        ) :: R where {D,N,L,P<:AbstractReciprocalPoint{D}, B<:AbstractBond{N,L}, R<:AbstractReciprocalUnitcell{P,B}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'newReciprocalUnitcell' for concrete reciprocal unitcell type " *
+            string(R) * " with bond type " * string(B) )
+end
