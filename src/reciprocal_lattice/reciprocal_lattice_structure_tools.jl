ab1c23c15c5e14ff6953bef06343a699a79234ff
Jan <jattig@thp.uni-koeln.de>
2018-11-24 11:42:17 +0100

    added reciprocal test function for unitcells

--- src/reciprocal_lattice/reciprocal_lattice_structure_tools.jl
+++ src/reciprocal_lattice/reciprocal_lattice_structure_tools.jl
@@ -14,7 +14,7 @@ function getReciprocalUnitcell(
                   LBR,BR<:AbstractBond{LBR,1},P<:AbstractReciprocalPoint{D},R<:AbstractReciprocalUnitcell{P,BR}}
 
     # build lattice vector
-    latvec_b1 = a1(unitcell) .* 1/sqrt(dot(a1(unitcell), a1(unitcell)))
+    latvec_b1 = a1(unitcell) .* 2*pi/sqrt(dot(a1(unitcell), a1(unitcell)))
 
     # return a new reciprocal unitcell
     return newReciprocalUnitcell(
@@ -32,10 +32,9 @@ function getReciprocalUnitcell(
     )
 end
 
+# 2d
 
-
-
-
+# 3d
 
 
 # export the function
@@ -44,7 +43,38 @@ export getReciprocalUnitcell
 
 
 
+
+
+
+
+# TEST IF TWO UNITCELLS ARE RECIPROCAL
+function testReciprocality(
+        uc_real :: U,
+        uc_reci :: R,
+        error_allowed :: Float64 = 1e-8
+    ) :: Bool where {N,UL,US,UB<:AbstractBond{UL,N},U<:AbstractUnitcell{US,UB},
+                     RL,RS,RB<:AbstractBond{RL,N},R<:AbstractReciprocalUnitcell{RS,RB}}
+
+    # the complete overlap
+    overlap_sum = 0.0
+
+    # test the overlap
+    for l1 in 1:length(latticeVectors(uc_real))
+    for l2 in 1:length(latticeVectors(uc_reci))
+        # the value that it should take
+        v_correct = l1==l2 ? 2*pi : 0.0
+        # calculate the value
+        v_taken = dot(latticeVectors(uc_real)[l1],latticeVectors(uc_reci)[l2])
+        # add to the overlap sum
+        overlap_sum += abs(v_correct - v_taken)
+    end
+    end
+
+    # check if the overall overlap is sufficiently small
+    return overlap_sum < error_allowed ? true : false
+end
+
+
 # TODO LIST
-# - getReciprocalUnitcell(unitcell)
 # - getBrillouinZone(rec_unitcell)
 # - getBrillouinZoneCorners(rec_unitcell)   (... edges, faces...)
