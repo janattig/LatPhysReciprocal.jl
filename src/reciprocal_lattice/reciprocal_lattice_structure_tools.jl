32a420d9c8b283a00817ce72b73d3c9965203e92
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-26 14:37:52 +0100

    added reciprocal unitcell building for d=2

--- src/reciprocal_lattice/reciprocal_lattice_structure_tools.jl
+++ src/reciprocal_lattice/reciprocal_lattice_structure_tools.jl
@@ -6,7 +6,9 @@
 
 # CONSTRUCT THE RECIPROCAL UNITCELL FROM A UNITCELL
 
-# 1d
+###########
+#   1D    #
+###########
 function getReciprocalUnitcell(
         ::Type{R},
         unitcell :: U
@@ -32,7 +34,102 @@ function getReciprocalUnitcell(
     )
 end
 
-# 2d
+
+###########
+#   2D    #
+###########
+
+# N=2, D<2 or D>3 (FALLBACK)
+function getReciprocalUnitcell(
+        ::Type{R},
+        unitcell :: U
+    ) :: R where {D,LS,LB,S<:AbstractSite{LS,D},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B},
+                  LBR,BR<:AbstractBond{LBR,2},P<:AbstractReciprocalPoint{D},R<:AbstractReciprocalUnitcell{P,BR}}
+
+    # give an error
+    error("Cannot build a reciprocal unitcell for realspace lattice dimensions D=" * string(D) * " and N=2.")
+end
+
+# N=2, D=2
+function getReciprocalUnitcell(
+        ::Type{R},
+        unitcell :: U
+    ) :: R where {LS,LB,S<:AbstractSite{LS,2},B<:AbstractBond{LB,1},U<:AbstractUnitcell{S,B},
+                  LBR,BR<:AbstractBond{LBR,1},P<:AbstractReciprocalPoint{2},R<:AbstractReciprocalUnitcell{P,BR}}
+
+    # build reciprocal lattice vectors
+    latvec_b1 = [a2(unitcell)[2], -a2(unitcell)[1]]
+    latvec_b2 = [a1(unitcell)[2], -a1(unitcell)[1]]
+
+    # normalize the reciprocal vectors
+    latvec_b1 .*= 2*pi/dot(latvec_b1, a1(unitcell))
+    latvec_b2 .*= 2*pi/dot(latvec_b2, a2(unitcell))
+
+    # return a new reciprocal unitcell
+    return newReciprocalUnitcell(
+        # pass the given type
+        R,
+        # pass the single lattice vector
+        Vector{Float64}[
+            latvec_b1,
+            latvec_b2
+        ],
+        # pass the two relevant bonds
+        BR[
+            newBond(BR, 1,1, getDefaultLabel(LBR), ( 0,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), ( 0,-1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1, 0)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1, 0)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1,-1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1,-1))
+        ]
+    )
+end
+
+# N=2, D=3
+function getReciprocalUnitcell(
+        ::Type{R},
+        unitcell :: U
+    ) :: R where {LS,LB,S<:AbstractSite{LS,3},B<:AbstractBond{LB,1},U<:AbstractUnitcell{S,B},
+                  LBR,BR<:AbstractBond{LBR,1},P<:AbstractReciprocalPoint{3},R<:AbstractReciprocalUnitcell{P,BR}}
+
+    # build a orthogonal vector
+    ortho = cross(a1(unitcell), a2(unitcell))
+
+    # build reciprocal lattice vectors
+    latvec_b1 = cross(a2(unitcell), ortho)
+    latvec_b2 = cross(a1(unitcell), ortho)
+
+    # normalize the reciprocal vectors
+    latvec_b1 .*= 2*pi/dot(latvec_b1, a1(unitcell))
+    latvec_b2 .*= 2*pi/dot(latvec_b2, a2(unitcell))
+
+    # return a new reciprocal unitcell
+    return newReciprocalUnitcell(
+        # pass the given type
+        R,
+        # pass the single lattice vector
+        Vector{Float64}[
+            latvec_b1,
+            latvec_b2
+        ],
+        # pass the two relevant bonds
+        BR[
+            newBond(BR, 1,1, getDefaultLabel(LBR), ( 0,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), ( 0,-1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1, 0)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1, 0)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1,+1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (+1,-1)),
+            newBond(BR, 1,1, getDefaultLabel(LBR), (-1,-1))
+        ]
+    )
+end
+
+
 
 # 3d
 
