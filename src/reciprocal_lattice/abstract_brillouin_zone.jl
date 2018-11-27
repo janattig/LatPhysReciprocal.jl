f76466e7b6efe4f2f2cfc9bca0ca68f14e299e66
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-27 13:53:10 +0100

    implemented abstract BZ interface

--- src/reciprocal_lattice/abstract_brillouin_zone.jl
+++ src/reciprocal_lattice/abstract_brillouin_zone.jl
@@ -61,10 +61,88 @@ function newBrillouinZone(
             string(BZ) * " with reciprocal unitcell type " * string(RU) )
 end
 
+# export constructor
+export newBrillouinZone
 
 
-# INTERFACE FUNCTIONS
-#
-# - corners(BZ), faces(BZ)
-# - rec_unitcell(BZ)
-#
+
+
+# accessing the corner points of the BZ
+function corners(
+            bz :: BZ
+        ) :: Vector{Vector{Float64}} where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'corners' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# setting the corner points of the BZ
+function corners!(
+            bz      :: BZ,
+            corners :: Vector{<:Vector{<:Real}}
+        ) where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'corners!' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# export corner interface
+export corners, corners!
+
+
+
+
+
+# accessing the faces of the BZ
+function faces(
+            bz :: BZ
+        ) :: Vector{Vector{Int64}} where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'faces' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# setting the faces of the BZ
+function faces!(
+            bz    :: BZ,
+            faces :: Vector{<:Vector{<:Integer}}
+        ) where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'faces!' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# export faces interface
+export faces, faces!
+
+
+
+
+
+# accessing the reciprocal unitcell of the BZ
+function reciprocalUnitcell(
+            bz :: BZ
+        ) :: RU where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'reciprocalUnitcell' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# setting the reciprocal unitcell of the BZ
+function reciprocalUnitcell!(
+            bz  :: BZ,
+            ruc :: RU
+        ) where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'reciprocalUnitcell!' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+# export reciprocalUnitcell interface
+export reciprocalUnitcell, reciprocalUnitcell!
