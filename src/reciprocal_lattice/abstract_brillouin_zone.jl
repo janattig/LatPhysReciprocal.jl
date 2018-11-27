8f6021ee49342d711e4ae91448342162dbda5006
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-27 13:10:37 +0100

    started implementation of abstract brillouin zones

--- src/reciprocal_lattice/abstract_brillouin_zone.jl
+++ src/reciprocal_lattice/abstract_brillouin_zone.jl
@@ -1,3 +1,68 @@
+################################################################################
+#
+#	ABSTRACT TYPE
+#
+#   BrillouinZone{R}
+#   --> R is the reciprocal unitcell type (<: AbstractReciprocalUnitcell{P,B})
+#       --> P is the reciprocal site type (<: AbstractReciprocalPoint{D})
+#           --> D is the dimension of embedding space
+#       --> B is the bond type (<: AbstractBond{L,N})
+#           --> N is the dimension of the Bravais lattice that the bond is located in
+#           --> L is the label type of bonds
+#
+#   FILE CONTAINS
+#       - abstract type definition
+#       - interface definition
+#       - TODO interface testing
+#
+################################################################################
+
+################################################################################
+#
+#   ABSTRACT TYPE DEFINITION
+#
+################################################################################
+abstract type AbstractBrillouinZone{
+        R <: AbstractReciprocalUnitcell{P,B} where {
+            P <: AbstractReciprocalPoint{D} where {D},
+            B <: AbstractBond{L,N} where {L,N},
+        }
+    } end
+
+# export the type
+export AbstractBrillouinZone
+
+
+
+
+
+
+
+
+
+################################################################################
+#
+#	INTERFACING / ACCESSING BRILLOUIN ZONES
+#	(functions that have to be overwritten by concrete types)
+#
+################################################################################
+
+
+# constructor
+function newBrillouinZone(
+            ::Type{BZ},
+            reciprocal_unitcell :: RU,
+            corners             :: Vector{<:Vector{<:Real}},
+            faces               :: Vector{<:Vector{<:Integer}}
+        ) :: BZ where {D,N,L,P<:AbstractReciprocalPoint{D},B<:AbstractBond{L,N},RU<:AbstractReciprocalUnitcell{P,B},BZ<:AbstractBrillouinZone{RU}}
+
+    # throw an error because implementation for concrete type is missing
+    error(  "not implemented function 'newBrillouinZone' for concrete Brillouin zone type " *
+            string(BZ) * " with reciprocal unitcell type " * string(RU) )
+end
+
+
+
 # INTERFACE FUNCTIONS
 #
 # - corners(BZ), faces(BZ)
