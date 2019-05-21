043cc4ff3d7ecece29d75648bcc7afea34f7e4b0
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-05-21 15:19:22 +0200

    added K and M point from BZ for 3d unitcells

--- src/reciprocal_points/default_points.jl
+++ src/reciprocal_points/default_points.jl
@@ -167,6 +167,64 @@ end
 
 
 
+# K point (Based on BZ object) in 2D
+function getReciprocalPoint(
+            :: Type{R},
+            unitcell   :: U,
+            identifier :: Val{:K},
+            instance   :: Val{I}
+        ) :: R where {D,I,N,LS,LB, R<:AbstractReciprocalPoint{3}, S<:AbstractSite{LS,3}, B<:AbstractBond{LB,N},U<:AbstractUnitcell{S,B}}
+
+    # get the bz
+    bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
+
+    # get the faceindex
+    faceindex = div(I, 10) + 1
+    face = faces(bz)[faceindex]
+    # first and second index
+    cornerindex = mod(I, 10)
+    i = face[mod(cornerindex, length(face))+1]
+
+    # return the specific type
+    return newReciprocalPoint(
+        R,
+        corners(bz)[i],
+        "K",
+        "K"
+    )
+end
+
+# K' point (Based on BZ object) in 2D
+function getReciprocalPoint(
+            :: Type{R},
+            unitcell   :: U,
+            identifier :: Val{:Kp},
+            instance   :: Val{I}
+        ) :: R where {D,I,N,LS,LB, R<:AbstractReciprocalPoint{3}, S<:AbstractSite{LS,3}, B<:AbstractBond{LB,N},U<:AbstractUnitcell{S,B}}
+
+    # get the bz
+    bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
+
+    # get the faceindex
+    faceindex = div(I, 10) + 1
+    face = faces(bz)[faceindex]
+    # first and second index
+    cornerindex = mod(I, 10)
+    i = face[mod(cornerindex+1, length(face))+1]
+
+    # return the specific type
+    return newReciprocalPoint(
+        R,
+        corners(bz)[i],
+        "K\'",
+        "K\'"
+    )
+end
+
+
+
+
+
 
 #########################
 #
@@ -186,8 +244,8 @@ function getReciprocalPoint(
     bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
 
     # first and second index
-    i1 = faces(bz)[1][mod(I-1,length(corners(bz))) + 1]
-    i2 = faces(bz)[1][mod(I,length(corners(bz))) + 1]
+    i1 = faces(bz)[1][mod(I-1,length(faces(bz)[1])) + 1]
+    i2 = faces(bz)[1][mod(I,length(faces(bz)[1])) + 1]
     # find the first and next corner
     c1 = corners(bz)[i1]
     c2 = corners(bz)[i2]
@@ -213,8 +271,72 @@ function getReciprocalPoint(
     bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
 
     # first and second index
-    i1 = faces(bz)[1][mod(I,length(corners(bz))) + 1]
-    i2 = faces(bz)[1][mod(I+1,length(corners(bz))) + 1]
+    i1 = faces(bz)[1][mod(I,length(faces(bz)[1])) + 1]
+    i2 = faces(bz)[1][mod(I+1,length(faces(bz)[1])) + 1]
+    # find the first and next corner
+    c1 = corners(bz)[i1]
+    c2 = corners(bz)[i2]
+
+    # return the specific type
+    return newReciprocalPoint(
+        R,
+        (c1.+c2)./2,
+        "M\'",
+        "M\'"
+    )
+end
+
+
+
+# M point (Based on BZ object) in 3D
+function getReciprocalPoint(
+            :: Type{R},
+            unitcell   :: U,
+            identifier :: Val{:M},
+            instance   :: Val{I}
+        ) :: R where {D,I,N,LS,LB, R<:AbstractReciprocalPoint{3}, S<:AbstractSite{LS,3}, B<:AbstractBond{LB,N},U<:AbstractUnitcell{S,B}}
+
+    # get the bz
+    bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
+
+    # get the faceindex
+    faceindex = div(I, 10) + 1
+    face = faces(bz)[faceindex]
+    # first and second index
+    cornerindex = mod(I, 10)
+    i1 = face[mod(cornerindex-1, length(face)) + 1]
+    i2 = face[mod(cornerindex,   length(face)) + 1]
+    # find the first and next corner
+    c1 = corners(bz)[i1]
+    c2 = corners(bz)[i2]
+
+    # return the specific type
+    return newReciprocalPoint(
+        R,
+        (c1.+c2)./2,
+        "M",
+        "M"
+    )
+end
+
+# M' point (Based on BZ object) in 3D
+function getReciprocalPoint(
+            :: Type{R},
+            unitcell   :: U,
+            identifier :: Val{:Mp},
+            instance   :: Val{I}
+        ) :: R where {D,I,N,LS,LB, R<:AbstractReciprocalPoint{3}, S<:AbstractSite{LS,3}, B<:AbstractBond{LB,N},U<:AbstractUnitcell{S,B}}
+
+    # get the bz
+    bz = getBrillouinZone(getReciprocalUnitcell(unitcell))
+
+    # get the faceindex
+    faceindex = div(I, 10) + 1
+    face = faces(bz)[faceindex]
+    # first and second index
+    cornerindex = mod(I, 10)
+    i1 = face[mod(cornerindex, length(face)) + 1]
+    i2 = face[mod(cornerindex+1,   length(face)) + 1]
     # find the first and next corner
     c1 = corners(bz)[i1]
     c2 = corners(bz)[i2]
